e3b79f559ac67a15105fd43fd218e7c2f6ecb169
HanderWei <hander_wei@163.com>
2015-02-03 14:00:54 +0800

    Solve Me First
    A+B problem

---
+++ SolveMeFirst.c
@@ -0,0 +1,17 @@
+//
+//  SolveMeFirst.c
+//  https://www.hackerrank.com/challenges/solve-me-first
+//  return A+B
+//
+//  Created by Hander on 15/2/3.
+//
+//
+
+#include <stdio.h>
+
+int main(){
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1 + num2);
+    return 0;
+}
